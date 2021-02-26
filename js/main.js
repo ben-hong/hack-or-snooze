c52583cf1124eda7df59b892f3e3fe93b79cd7d2
Ben Hong <benhong654@gmail.com>
2021-02-25 21:08:47 -0800

    testing branching

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 "use strict";
-
+//tester
 // So we don't have to keep re-finding things on page, find DOM elements once:
-
 const $body = $("body");
 
 const $storiesLoadingMsg = $("#stories-loading-msg");
